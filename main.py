8bfef7984348fbc1e338b94d4b519677aba7a42d
RWallan <3am.richardwallan@gmail.com>
2024-05-24 22:44:35 -0300

    feat: Utilizando discord.ext para os comandos
    O discord já disponibiliza uma funcionalidade para lidar com os
    comandos. Desta forma o código fica mais limpo e fácil de estender.
    
    - responses se tornou commands
    - a classe client agora utiliza-se MyBot
    - os comandos funcionam como rotas e são adicionadas com bot.add

--- main.py
+++ main.py
@@ -1,64 +1,39 @@
 import os
 
-from discord import Client, Intents, Message
+from discord import Intents
+from discord.ext import commands
 from dotenv import find_dotenv, load_dotenv
 
-from responses import get_response
+from commands import hello, roadmap, roll_dice
 
 # GET TOKEN
-load_dotenv(find_dotenv('.venv/.env'))
+load_dotenv(find_dotenv('.env'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 # Configurando as necessidades do bot: Ler e enviar mensagens
 intents = Intents.default()
 intents.message_content = True
-client = Client(intents=intents)
 
 
-# Funcionalidades de mensagem
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message:
-        print('Intents não foram configurados propriamente.')
-        return
+class MyBot(commands.Bot):
+    """Estende a classe base Bot."""
 
-    # '?' é usado quando o usuário deseja receber a resposta no privado.
-    if is_private := user_message[0] == '?':
-        user_message = user_message[1:]
+    async def on_ready(self):
+        """Evento de inicialização."""
+        print(f'{self.user} está rodando')
 
-    try:
-        response = get_response(user_message)
-        await message.author.send(
-            response
-        ) if is_private else await message.channel.send(response)
-    except Exception as e:
-        print(e)
+    async def on_command_error(self, ctx, error) -> None:
+        """Lida com erros no comando."""
+        print(error)
+        if isinstance(error, commands.errors.CommandError):
+            await ctx.send('Alguma coisa deu errado ao executar esse comando.')
 
 
-# Manuseando a inicialização e funcionamento do Bot
-@client.event
-async def on_read() -> None:
-    print(f'{client.user} está rodando')
-
-
-@client.event
-async def on_message(message: Message) -> None:
-    # Verifica se o autor da mensagem é o próprio Bot
-    if message.author == client.user:
-        return
-
-    username = str(message.author)
-    user_message = message.content
-    channel = str(message.channel)
-
-    if user_message[0] == '=':
-        print(f'[{channel}] {username}: "{user_message[1:]}"')
-        await send_message(message, user_message[1:])
-
-
-# Main  entry point
-def main() -> None:
-    client.run(token=TOKEN)
+bot = MyBot(command_prefix='=', intents=intents)
+bot.add_command(hello)
+bot.add_command(roll_dice)
+bot.add_command(roadmap)
 
 
 if __name__ == '__main__':
-    main()
+    bot.run(TOKEN)

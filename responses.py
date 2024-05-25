50ab3953765037a3a1b6ef5e484aec1770d6e92f
RWallan <3am.richardwallan@gmail.com>
2024-05-24 21:56:10 -0300

    style: Style code

--- responses.py
+++ responses.py
@@ -1,14 +1,13 @@
-from random import choice, randint
+from random import randint
+
 import requests
 
-URL='https://raw.githubusercontent.com/rafaeltiribas/techtiribas/main/roadmap/README.md'
-
-
+URL = 'https://raw.githubusercontent.com/rafaeltiribas/techtiribas/main/roadmap/README.md'
 
 
 def get_response(user_input: str) -> str:
     lowered = user_input.lower()
-    
+
     if lowered == '':
         return 'Mensagem vazia'
     elif 'salve' in lowered:

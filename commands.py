8982c845c195246615ee1b7442f401f0b406b0bb
Chirana <engsw.thiago@gmail.com>
2024-05-29 00:36:41 -0300

    feat: Ajustes para usar slash commands

--- commands.py
+++ commands.py
@@ -7,24 +7,25 @@ Este parâmetro está sendo chamado convencionalmente de `ctx`.
 from random import randint
 
 import requests
-from discord.ext import commands
+from discord.ext import commands as com
+import time
 
 ROADMAP_URL = 'https://raw.githubusercontent.com/rafaeltiribas/techtiribas/main/roadmap/README.md'
 
 
-@commands.command('salve', help='Responde uma saudação.')
-async def hello(ctx):
+@com.hybrid_command(help='Responde uma saudação.')
+async def salve(ctx):
     """Responde uma saudação."""
     await ctx.send('Salvi')
 
 
-@commands.command('dice', help='Rola um dado.')
-async def roll_dice(ctx):
+@com.hybrid_command(help='Rola um dado.')
+async def dice(ctx):
     """Rola um dado."""
     await ctx.send(randint(1, 6))
 
 
-@commands.command('roadmap', help='Mostra o roadmap das lives.')
+@com.hybrid_command(help='Mostra o roadmap das lives.')
 async def roadmap(ctx):
     """Mostra o roadmap das lives."""
     await ctx.send(requests.get(ROADMAP_URL))

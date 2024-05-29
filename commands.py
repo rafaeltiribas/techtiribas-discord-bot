56bf90631ad2c5e4b1faec928b54d37f1eaaa268
Chirana <engsw.thiago@gmail.com>
2024-05-29 00:36:56 -0300

    feat: o clássico /ping

--- commands.py
+++ commands.py
@@ -29,3 +29,21 @@ async def dice(ctx):
 async def roadmap(ctx):
     """Mostra o roadmap das lives."""
     await ctx.send(requests.get(ROADMAP_URL))
+
+
+@com.hybrid_command(help="Dá um ping, toma um pong")
+async def ping(ctx: com.Context):
+    """Clássica resposta depois de um ping"""
+    start_time = time.time()
+    msg = await ctx.send("Calculando ping...")
+    end_time = time.time()
+    ms = (end_time - start_time) * 1000
+
+    await msg.edit(content=f"pong! Latência está em {ms:.2f} ms")
+
+def setup(bot):
+    """defina aqui os comandos no bot"""
+    bot.add_command(ping)
+    bot.add_command(roadmap)
+    bot.add_command(dice)
+    bot.add_command(salve)

a85dd45017e703baf294f6f944eab3348103a7ea
Chirana <engsw.thiago@gmail.com>
2024-05-31 23:35:52 -0300

    feat: Ajustes no registro

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -2,22 +2,19 @@ from src.models.user import User
 from src.models.wallet import Wallet
 from src.models.transaction_history import TransactionHistory, TransactionType
 import db.database_config as db
-
 class UserService:
 
 	def register_user_from_ctx(self, ctx):
 		con = db.open_transaction()
 		try:
-			discord_id = str(ctx.author.id)
-			user_found = User.selectBy(id_discord=discord_id)
-			if user_found is not None:
+			if self.get_user_by_ctx(ctx) is not None:
 				return 'Você já está cadastrado! use **/** para ver outros comandos'
-			user_id = discord_id
+			user_id = str(ctx.author.id)
 			username = ctx.author.name
 			user = self.add_user(user_id, username, User.ROLE["Member"])
 			wallet = self.create_wallet_for_user(user)
 			con.commit()
-			return f'{user.id_discord} Resgistrado! {user.username} possui {wallet.balance} Bytes!'
+			return f'Você foi Resgistrado! agora você possui uma carteira com {wallet.balance} Bytes!'
 		except Exception as e:
 			print("Houve erro ao cadastrar novo user: "+e)
 			con.rollback()

da83f43668d261ae8eb34b86408e873809518c82
Chirana <engsw.thiago@gmail.com>
2024-06-02 23:04:32 -0300

    feat: adicionando persistencia de Interacoes de User

---
+++ src/models/user_interactions.py
@@ -0,0 +1,9 @@
+from sqlobject import SQLObject, StringCol, ForeignKey, DateTimeCol
+
+class UserInteractions(SQLObject):
+	cron_execute_periodic_cleaning = StringCol()
+
+class UserInteractionsHistory(SQLObject):
+	user = ForeignKey('User')
+	time_iteracted = DateTimeCol()
+	
---
+++ src/services/user_interactions_service.py
@@ -0,0 +1,30 @@
+from src.models.user_interactions import UserInteractionsHistory, UserInteractions
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+
+class UserInteractionsService:
+	
+	def all_interactions(self):
+		return UserInteractionsHistory.select()
+	
+	def all_users_who_interacted(self):
+		all_interactions = self.all_interactions()
+		users = [interaction.user for interaction in all_interactions]
+		return users
+	
+	
+	
+	"""Inicia schedules"""
+	def start_schedules(self):
+		self.sched = BackgroundScheduler()
+		user_interaction_config = UserInteractions.select()
+		self.trigger = CronTrigger.from_crontab(user_interaction_config.cron_execute_periodic_cleaning)
+		self.execute_cron_tasks()
+		
+	def execute_cron_tasks(self):
+		self.sched.add_job(self.realize_periodic_cleaning, self.trigger)
+		self.sched.start()
+	
+	def realize_periodic_cleaning(self):
+		UserInteractionsHistory.deleteAll()
+		print("Limpeza de interacoes dos usuários foram feitas com sucesso")

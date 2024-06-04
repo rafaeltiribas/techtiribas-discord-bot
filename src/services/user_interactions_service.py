53f334f3f1e0d37552f11629d2d44508b0ca94a6
Chirana <engsw.thiago@gmail.com>
2024-06-03 21:26:13 -0300

    feat: registrar interacao do User para prizes

--- src/services/user_interactions_service.py
+++ src/services/user_interactions_service.py
@@ -1,10 +1,21 @@
 from src.models.user_interactions import UserInteractionsHistory, UserInteractions
+from src.models.user import User
+import src.utils.cron_expressions as cron_exp
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
 import src.utils.log as LOG
 
 class UserInteractionsService:
+		
+	def init_bank(self):
+		self.user_interaction_config = UserInteractions.select().getOne(None)
+		if self.user_interaction_config is None:
+			cron = "0 3 * * *"
+			LOG.info(f'Configuracao de limpeza de interacoes do usuário marcada para {cron_exp.get_datetime(cron)}')
+			self.user_interaction_config = UserInteractions(
+				cron_execute_periodic_cleaning=cron
+			)
 	
 	def all_interactions(self):
 		return UserInteractionsHistory.select()
@@ -14,18 +25,27 @@ class UserInteractionsService:
 		users = [interaction.user for interaction in all_interactions]
 		return users
 	
+	def add_user(self, user: User):
+		UserInteractionsHistory(user=user, time_iteracted=datetime.now())
 	
+	def has_already_interacted(self, user: User):
+		interactions = UserInteractionsHistory.selectBy(user=user)
+		if interactions is None:
+			return False
+		else:
+			return True
 	
 	"""Inicia schedules"""
 	def start_schedules(self):
 		self.sched = BackgroundScheduler()
-		user_interaction_config = UserInteractions.select()
-		self.trigger = CronTrigger.from_crontab(user_interaction_config.cron_execute_periodic_cleaning)
+		self.trigger = CronTrigger.from_crontab(self.user_interaction_config.cron_execute_periodic_cleaning)
 		self.execute_cron_tasks()
 		
 	def execute_cron_tasks(self):
-		self.sched.add_job(self.realize_periodic_cleaning, self.trigger)
-		self.sched.start()
+		id = "periodic_cleaning"
+		if not self.sched.get_job(id):
+			self.sched.add_job(self.realize_periodic_cleaning, self.trigger, id=id)
+			self.sched.start()
 	
 	def realize_periodic_cleaning(self):
 		UserInteractionsHistory.deleteAll()
